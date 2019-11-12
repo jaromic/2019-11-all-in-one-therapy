2d9a9c248348a7af67edf3aa7ff60b2a87b656ea
Michael Jaros <michael.jaros@jarosoft.at>
2019-11-12 13:10:34 +0100

    bootstrap framework added

--- resources/views/backend/base.blade.php
+++ resources/views/backend/base.blade.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
