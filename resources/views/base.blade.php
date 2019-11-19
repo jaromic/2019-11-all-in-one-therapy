027494480946d318a17e5fef9577743a7625b705
Michael Jaros <michael.jaros@jarosoft.at>
2019-11-19 09:00:43 +0100

    message on invalid login, bootstrap (cdn) added

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -11,6 +11,7 @@
 
         <!-- Styles -->
         <link href="/css/app.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,8 +6,13 @@
     {{-- Login-Formular --}}
     <form method="post" action="{{ route('authenticate') }}">
         @csrf
+        @if( $errors->any() )
+            <p class="alert alert-danger">Die Zugangsdaten sind nicht korrekt.</p>
+        @endif
         <p><input type="text" name="name" placeholder="Benutzer"></p>
         <p><input type="password" name="password" placeholder="Passwort"></p>
-        <p><button type="submit">Login</button></p>
+        <p>
+            <button type="submit">Login</button>
+        </p>
     </form>
 @endsection
